1fd66bc0b2f3bc46f28925e27d2e4863a3967218
Jose Javier <josejavier.gonzalez.ortiz@gmail.com>
2020-07-01 20:50:41 -0700

    Streamline job submission for distributed jobs

--- experiment/submit.py
+++ experiment/submit.py
@@ -1,3 +1,9 @@
+import os
+import math
+import pathlib
+import yaml
+
+import torch
 import submitit
 import torch.distributed as dist
 
@@ -7,28 +13,22 @@ from geth.experiment import DistributedExperiment
 
 class CheckpointWrapper:
 
-    def __init__(self, experiment_class):
+    def __init__(self, experiment_class, cluster_params):
         self.experiment_class = experiment_class
+        self.cluster_params = cluster_params.copy()
 
     def __call__(self, **kwargs):
 
+        # Log cluster params
+        cp_str = ('\n' + yaml.dump(self.cluster_params)).replace('\n', '\n\t')
+        printc("Running with cluster params" + cp_str, color='CYAN')
+
         if issubclass(self.experiment_class, DistributedExperiment):
-            job_env = submitit.JobEnvironment()
-            master_node = job_env.hostnames[0]
-            # import os; os.environ['CUDA_VISIBLE_DEVICES'] = str(job_env.global_rank)  # TODO REMOVE
-            kwargs['distributed'] = dict(
-                global_rank=job_env.global_rank,
-                local_rank=job_env.local_rank,
-                num_nodes=job_env.num_nodes,
-                num_tasks=job_env.num_tasks,
-                node=job_env.node,
-                master=master_node
-            )
-            self.distributed = kwargs['distributed']
-            tcp = f'tcp://{master_node}:42029'
-            dist.init_process_group(init_method=tcp, rank=job_env.global_rank, world_size=job_env.num_tasks, backend='nccl')
+            self.distributed_setup()
+            kwargs['env'] = self.distributed
 
         self.experiment = self.experiment_class(**kwargs)
+        self.save_cluster_params()
 
         if not self.experiment.path.exists():
             self.experiment.run()
@@ -36,28 +36,92 @@ class CheckpointWrapper:
             self.experiment.load()
             self.experiment.resume()
 
+    def distributed_setup(self):
+        job_env = submitit.JobEnvironment()
+        master_node = job_env.hostnames[0]
+        self.distributed = dict(
+            global_rank=job_env.global_rank,
+            local_rank=job_env.local_rank,
+            num_nodes=job_env.num_nodes,
+            num_tasks=job_env.num_tasks,
+            node=job_env.node,
+            master=master_node
+        )
+        tcp = f'tcp://{master_node}:42029'
+        # Init torch.distributed WORLD group
+        dist.init_process_group(init_method=tcp, rank=job_env.global_rank, world_size=job_env.num_tasks, backend='nccl')
+        # GPU isolation
+        os.environ['CUDA_VISIBLE_DEVICES'] = str(job_env.local_rank)
+
     def checkpoint(self, **kwargs):
         printc("SubmitIt checkpoint", color='ORANGE')
-        resubmit = CheckpointWrapper(self.experiment_class)
-        path = self.experiment.path.as_posix()
+        resubmit = CheckpointWrapper(self.experiment_class, self.cluster_params)
+        path = self.experiment.path
+
         # Only the master node gets the checkpoint and requeue signal
         if issubclass(self.experiment_class, DistributedExperiment):
             # Need to use parent path for the whole resubmission
-            path = self.experiment.parent_path.as_posix()
-            if self.experiment.distributed['global_rank'] != 0:
+            path = self.experiment.parent_path
+
+        self.experiment.checkpoint(tag='interrupt')
+
+        return submitit.helpers.DelayedSubmission(resubmit, path=path.as_posix())
+
+    def save_cluster_params(self):
+        path = self.experiment.path
+        if isinstance(self.experiment, DistributedExperiment):
+            path = self.experiment.parent_path
+            if not self.experiment.is_master:
                 return
-        else:
-            self.experiment.checkpoint(tag='interrupt')
-
-        return submitit.helpers.DelayedSubmission(resubmit, path=path)
+        with open(path / 'cluster_params.yml', 'w') as f:
+            yaml.dump(self.cluster_params, f)
 
 
-def submit_experiment(cluster_params, experiment, debug=False, **kwargs):
+def submit_experiment(experiment, debug=False, cluster_params=None, **kwargs):
+    _cluster_params = dict(
+        timeout_min=60 * 8,
+        slurm_partition='learnfair',
+        gpus_per_node=1,
+        cpus_per_task=10,
+        tasks_per_node=1,
+        nodes=1,
+        slurm_mem='64G'
+    )
+
+    if issubclass(experiment, DistributedExperiment):
+        # DistributedExperiments must have been initialized to prevent UID randomization
+        nodes, gpus_per_node, tasks_per_node = default_cluster_params_distributed(kwargs['path'])
+        _cluster_params.update(nodes=nodes, gpus_per_node=gpus_per_node, tasks_per_node=tasks_per_node)
+
+    if cluster_params is not None:
+        _cluster_params.update(cluster_params)
+
     if debug:
         executor = submitit.LocalExecutor(folder='/tmp/submitit-logs')
     else:
         executor = submitit.AutoExecutor(folder='/checkpoint/jjgo/submitit-logs/')
-    executor.update_parameters(**cluster_params)
-    submit = CheckpointWrapper(experiment)
+
+    # PyTorch 1.5 specific issue: https://github.com/pytorch/pytorch/issues/37377
+    if torch.__version__ == '1.5.0':
+        os.environ['MKL_THREADING_LAYER'] = 'GNU'  # Workaround
+
+    executor.update_parameters(**_cluster_params)
+    submit = CheckpointWrapper(experiment, _cluster_params)
     job = executor.submit(submit, **kwargs)
+    print(f"Submitted job {job.job_id}")
     return job
+
+
+def default_cluster_params_distributed(path):
+    cfg_path = pathlib.Path(path) / 'config.yml'
+    with open(cfg_path, 'r') as f:
+        cfg = yaml.load(f, Loader=yaml.FullLoader)
+    world_size = cfg['distributed']['world_size']
+    nodes = math.ceil(world_size / 8)
+    gpus_per_node = min(8, world_size)
+    tasks_per_node = gpus_per_node
+    assert world_size % gpus_per_node == 0, \
+        f"GPUs-per-node {gpus_per_node} does not evenly divide num tasks {world_size}"
+    assert world_size == nodes * tasks_per_node, \
+        f"World size {world_size} does not equal to number of tasks {nodes}nodes x {tasks_per_node}tasks/node"
+    return nodes, gpus_per_node, tasks_per_node
